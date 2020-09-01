be53756e0322d38c6113538471e02c58c3a4e513
miyahara1005 <miyaharaaimi1005@gmail.com>
2020-09-01 12:27:33 +0900

    はじめてのfor in

---
+++ sample07.py
@@ -0,0 +1,12 @@
+#繰り返し
+#全員の名前を順に出力するには
+
+members = ['Bob', 'Tom', 'Ken', 'John', 'Alice']
+# print(members[0])
+# print(members[1])
+# print(members[2])
+# print(members[3])
+
+#  forを使ってスッキリ
+for member in members:
+    print(member)
