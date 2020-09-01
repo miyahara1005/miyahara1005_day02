6d53626f0aeacc3163352b170e2b5e4aa47c7746
miyahara1005 <miyaharaaimi1005@gmail.com>
2020-09-01 10:55:56 +0900

    データ型によって演算の結果が変わる話。「データ型のはたらき」Typeerror

---
+++ sample02.py
@@ -0,0 +1,15 @@
+# データ型によって演算の結果が変わる話（データ型のはたらき）
+
+print(3 + 4)
+print('baseball' + 'succer')  #文字同士の連結になる
+
+# *
+print(3 * 4)
+print('ピザ'* 5) #ピザピザピザピザピザ
+
+print('テーブルテニス' - 'テニス')
+#TypeError: unsupported operand type(s) for -: 'str' and 'str'
+
+print('3' + '野球')
+# TypeError: unsupported operand type(s) for -: 'str' and 'str'
+
