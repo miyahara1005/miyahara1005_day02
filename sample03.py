8cb2ad6c1b5a6707e2946ef386c9642bdfeb3afe
miyahara1005 <miyaharaaimi1005@gmail.com>
2020-09-01 11:20:08 +0900

    List型の定義とインデックスでアクセスできる

---
+++ sample03.py
@@ -0,0 +1,11 @@
+# List型
+members = ['Bob', 'Tom', 'Ken']
+
+print(type(members)) #<class 'list'>
+print(members) #['Bob', 'Tom', 'Ken']
+
+# Listの　要素　へのアクセス
+# インデックス（添字）によるアクセス
+print(members[0]) #Bob
+print(members[1]) #Tom
+print(members[2]) #Ken
