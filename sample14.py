af47e93690f870fd6da1e89865f2573620e5d6af
miyahara1005 <miyaharaaimi1005@gmail.com>
2020-09-01 15:34:13 +0900

    True と　False bool型真偽型　比較演算子のこたえ

---
+++ sample14.py
@@ -0,0 +1,12 @@
+#2 与えられた整数の偶奇を返す関数　is_even
+# True と Falseは　bool型（真偽値型）というデータ型
+print(type(True)) #<class 'bool'>
+print(type(False)) #<class 'bool'>
+
+# Q.
+print(10 == 10) #True
+print(7 > 3) #True
+print(222 % 2 == 0) #True
+print(5 % 2 == 0) #False
+print(10 != 10) #False
+print('bob' == 'Tom') #False
