16c03315b5df0f077cf16441f4436fba71dfb890
miyahara1005 <miyaharaaimi1005@gmail.com>
2020-09-01 10:37:16 +0900

    type()でデータ型を調べることができる！

---
+++ sample01.py
@@ -0,0 +1,20 @@
+# データ型
+name = 'Bob'
+
+print(name)
+
+# type() という命令でデータ型確認
+print(type(name)) # <class 'str'>
+print(type('hello')) # <class 'str'>
+print(type('saga')) # <class 'str'>
+print(type('japan')) # <class 'str'>
+
+# str はStringの略で　「文字列」のという意味！（プログラミング文脈では）
+# 文字列というのは　文字　の　列
+
+# じゃあ、整数でも試してみよう
+print(type(3))　#<class 'int'>
+print(type(1032))　#<class 'int'>
+print(type(12345))　#<class 'int'>
+
+# int は　Integerの略で「整数」という意味です！
